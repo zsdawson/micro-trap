3d8ecc9adc9b928547e92676eed586584cdeb063
zsdawson <65880961+zsdawson@users.noreply.github.com>
2024-01-20 01:24:54 -0800

    Update test-relay.py

--- pico-code/test-relay.py
+++ pico-code/test-relay.py
@@ -1,1 +1,16 @@
+from machine import Pin
+import utime
 
+# Initialize Relay
+relay_pin = Pin(15, Pin.OUT)
+
+def toggle_relay():
+    while True:
+        relay_pin.value(1)  # Turn on relay
+        utime.sleep(15)     # Wait for 15 seconds
+
+        relay_pin.value(0)  # Turn off relay
+        utime.sleep(15)     # Wait for 15 seconds
+
+# Start toggling the relay
+toggle_relay()
