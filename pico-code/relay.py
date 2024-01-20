eab4762186537b14bf5549012be92472db8dc0f5
zsdawson <65880961+zsdawson@users.noreply.github.com>
2024-01-19 18:15:27 -0800

    Update relay.py

--- pico-code/relay.py
+++ pico-code/relay.py
@@ -1,18 +1,13 @@
 from machine import Pin
 import utime
 
-# Set up the relay pin
 relay_pin = Pin(16, Pin.OUT)
 
-# Function to turn on the relay for a specified duration
-def turn_on_relay(duration):
-    relay_pin.high()  # Turn on the relay
-    try:
-        utime.sleep(duration)  # Keep the relay on for the specified duration
-    except KeyboardInterrupt:
-        print("Operation cancelled by user.")
-    finally:
-        relay_pin.low()  # Turn off the relay to ensure it is off
+def activate_relay(duration):
+    print("Relay ON")
+    relay_pin.high()
+    utime.sleep(duration)
+    print("Relay OFF")
+    relay_pin.low()
 
-
-turn_on_relay(15)
+activate_relay(15)
